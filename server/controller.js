093045e92ea3f32b1845f57f49564df3937d7107
BauteNonSinger <bautista19298@gmail.com>
2024-02-21 08:45:10 +0100

    start

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
 const expressAsyncHandler = require('express-async-handler')
+const urlModel = require('./model');
 
 function test(input) {    
     let res = {
@@ -8,8 +9,19 @@ function test(input) {
     return res;
 }
 
-function getUrl(input) {
+const getUrl = expressAsyncHandler(async(req, res) => {
 
-}
+})
+
+const postUrl = expressAsyncHandler(async(req, res) => {
+    const {url} = req.body;
+
+    if (!url) {
+        res.sendStatus(400);
+        throw new Error('No se ha proporcionado una URL')
+    }
+
+    const newUrl = await urlModel.create({baseUrl, shortUrl})
+})
 
 module.exports = {test, getUrl};
